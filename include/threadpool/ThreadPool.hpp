44f17e8e695682dfa9526b3b6e29071ba885fa93
wxy <wangxinyan1123@gmail.com>
2025-06-11 01:29:39 +0800

    update thread pool

--- include/threadpool/ThreadPool.hpp
+++ include/threadpool/ThreadPool.hpp
@@ -8,6 +8,7 @@
 #include <condition_variable>
 #include <type_traits>
 #include <vector>
+#include <unordered_map>
 
 namespace ThreadPoolManual {
 
@@ -17,20 +18,115 @@ enum class PoolMode
     MODE_FIXED
 };
 
-class Result;
+class Semaphore
+{
+public:
+    Semaphore(int semanum);
+    ~Semaphore();
+    void wait();
+    void post();
+
+private:
+    int semaNum_;
+    std::mutex semaphoreMtx_;
+    std::condition_variable isZero_;
+};
+
+class Any
+{
+public:
+    Any() = default;
+    ~Any() = default;
+    Any(const Any&) = delete;
+    Any& operator = (const Any&) = delete;
+    Any(Any&&) = default;
+    Any& operator = (Any&&) = default;
+
+    template<typename T>
+    T cast_()
+    {
+        Derive<T>* p = dynamic_cast<Derive<T>*>(base_.get());
+        if (!p)
+        {
+            std::cerr << "error type casting. " << std::endl;
+            throw "error";
+        }
+        return p->data_;
+    }
+private:
+    class Base
+    {
+        public:
+            virtual ~Base() = default;
+    };
+    // 模板派生类
+    template<typename T>
+    class Derive : public Base
+    {
+        public:
+            Derive(T data)
+                : data_(data)
+            {}
+            ~Derive() = default;
+        private:
+            T data_;
+    };
+    // 指向派生类的基类指针
+    std::unique_ptr<Base> base_;
+};
+
+class Task;
+
+class Result
+{
+public:
+    Result(std::shared_ptr<Task> sp, bool isvalid);
+    ~Result();
+    void setValue(Any val);
+    Any get();
+
+private:
+    Any anyValue_;
+    Semaphore sem_;
+    std::shared_ptr<Task> task_;
+    std::atomic_bool isValid_;
+};
 
 class Task
-{};
+{
+public:
+    Task();
+    ~Task();
+    virtual Any run();
+    void exec();
+    void setResult(Result* res);
+
+private:
+    Result* result_;
+
+};
 
 class Thread
-{};
+{
+public:
+    // 注意这里加速int的细节！
+    Thread(std::function<void(int)> func);
+    ~Thread();
+
+    void start();
+    int getThreadId() const;
+private:
+    std::function<void(int)> func_;
+    static int generateId_;
+    int threadId_;
+};
 
 //线程池类->应该不是模板类吧
 class ThreadPool
 {
 public:
-    ThreadPool() = default;
-    ~ThreadPool() = default;
+    ThreadPool();
+    ~ThreadPool();
 
     void setMode(PoolMode mode);
     void setMaxThreadNum(int number);
@@ -42,14 +138,16 @@ public:
     ThreadPool& operator = (const ThreadPool&) = delete;
 
 private:
-    void threadFunc();
+    void threadFunc(int threadid);
     bool checkRunningState();
 
 private:
-    std::vector<std::unique_ptr<Thread>> threads_;
+    // std::vector<std::unique_ptr<Thread>> threads_;
+    std::unordered_map<int, std::unique_ptr<Thread>> threads_;
     int initThreadSize_;
     std::atomic_int curThreadSize_;
-    int idleThreadSize_;
+    std::atomic_int idleThreadSize_;
+    int threadSizeMaxThreshold_;
 
     std::queue<std::shared_ptr<Task>> taskQueue_;
     std::atomic_int taskQueSize_;
@@ -57,6 +155,7 @@ private:
     
     std::condition_variable notEmpty_;
     std::condition_variable notFull_;
+    std::condition_variable exitCondition_;
     std::mutex taskQueueMtx_;
     // TODO
     std::atomic_bool isPoolRunning_;
--- src/threadpool/ThreadPool.cpp
+++ src/threadpool/ThreadPool.cpp
@@ -1,12 +1,103 @@
-
 #include <chrono>
 #include <functional>
 #include <mutex>
+#include <thread>
 #include "threadpool/ThreadPool.hpp"
 
 namespace ThreadPoolManual {
 
-constexpr int TASK_QUEUE_MAX_SIZE = 1024;
+constexpr int TASK_QUEUE_MAX_SIZE = INT32_MAX;
+constexpr int THREAD_MAX_IDLE_TIME = 60;
+
+Semaphore::Semaphore(int semnum = 0)
+    : semaNum_(semnum)
+{}
+
+Semaphore::~Semaphore() = default;
+
+void Semaphore::post()
+{
+    std::unique_lock<std::mutex> lock(semaphoreMtx_);
+    semaNum_++;
+    isZero_.notify_all();
+}
+
+void Semaphore::wait()
+{
+    std::unique_lock<std::mutex> lock(semaphoreMtx_);
+    isZero_.wait(lock, [&]()->bool { return semaNum_ > 0; });
+}
+
+Result::Result(std::shared_ptr<Task> task,
+                bool isvalid = false)
+    : isValid_(isvalid)
+    , task_(task)
+{
+    task_->setResult(this);
+    Semaphore sem_;
+}
+
+Result::~Result()
+{}
+
+void Result::setValue(Any anyval)
+{
+    anyValue_ = std::move(anyval);
+    sem_.post();
+}
+
+Any Result::get()
+{
+    if (!this->isValid_)
+    {
+        std::cerr << "task submission error occurred..." << std::endl;
+        throw "error getting result!";
+    }
+    sem_.wait();
+    return std::move(this->anyValue_);
+}
+
+Task::Task()
+    :result_(nullptr)
+{}
+
+Task::~Task()
+{}
+
+void Task::exec()
+{
+    if (result_ != nullptr)
+    {
+        this->result_->setValue(this->run());
+    }
+}
+
+void Task::setResult(Result* res)
+{
+    this->result_ = res;
+}
+
+Thread::Thread(std::function<void(int)> func)
+    : func_(func)
+    , threadId_(generateId_++)
+{}
+
+Thread::~Thread()
+{}
+
+int Thread::generateId_ = 0;
+
+int Thread::getThreadId() const
+{
+    return this->threadId_;
+}
+
+void Thread::start()
+{
+    std::thread t(func_, threadId_);
+    t.detach();
+}
+
 
 ThreadPool::ThreadPool()
     : initThreadSize_(0)
@@ -16,11 +107,15 @@ ThreadPool::ThreadPool()
     , isPoolRunning_(false)
     , poolMode_(PoolMode::MODE_FIXED)
     , taskQueSize_(0)
-    // , threads_(std::vector)
 {}
 
 ThreadPool::~ThreadPool()
-{}
+{
+    isPoolRunning_ = false;
+    std::unique_lock<std::mutex> lock(taskQueueMtx_);
+    notEmpty_.notify_all();
+    exitCondition_.wait(lock, [&]()->bool { return threads_.size() == 0; });
+}
 
 void ThreadPool::setMode(PoolMode mode)
 {
@@ -36,30 +131,38 @@ Result ThreadPool::submitTask(std::shared_ptr<Task> sp)
 {
     std::unique_lock<std::mutex> lock(taskQueueMtx_);
     if (!notFull_.wait_for(lock, std::chrono::seconds(1),
-            [&]()->bool { return taskQueue_.size() < taskQueMaxThreshold_; }))
+            [&]()->bool { return taskQueue_.size() < (size_t)taskQueMaxThreshold_; }))
     {
         std::cerr << "task queue is full, fail submission." << std::endl;
         return Result(sp, false);
     }
-    if (poolMode_ == PoolMode::MODE_CACHED)
-    {
-        
-    }
-
     taskQueue_.emplace(sp);
     taskQueSize_++;
     notEmpty_.notify_all();
-
+    if (poolMode_ == PoolMode::MODE_CACHED
+        && curThreadSize_ < threadSizeMaxThreshold_
+        && taskQueSize_ > idleThreadSize_)
+    {
+        // create thread
+        auto ptr = std::make_unique<Thread>(std::bind(&ThreadPool::threadFunc, this, std::placeholders::_1));
+        int threadId = ptr->getThreadId();
+        threads_.emplace(threadId, std::move(ptr));
+        // start thread
+        threads_[threadId]->start();
+        curThreadSize_++;
+        idleThreadSize_++;
+    }
     return Result(sp, true);
 }
 
 void ThreadPool::start(int threadSize)
 {
+    isPoolRunning_ = true;
     initThreadSize_ = threadSize;
     for (int i = 0; i < initThreadSize_; i++)
     {
-        auto ptr = std::make_unique<Thread>(std::bind(&ThreadPool::threadFunc, this));
-        threads_.emplace_back(std::move(ptr));
+        auto ptr = std::make_unique<Thread>(std::bind(&ThreadPool::threadFunc, this, std::placeholders::_1));
+        threads_.emplace(ptr->getThreadId(), std::move(ptr));
     }
     for (int i = 0; i < initThreadSize_; i++)
     {
@@ -67,45 +170,77 @@ void ThreadPool::start(int threadSize)
         ++idleThreadSize_;
         ++curThreadSize_;
     }
-
-
 }
 
-void ThreadPool::threadFunc()
+void ThreadPool::threadFunc(int threadid)
 {
-    // 启动线程执行函数
+    auto lastTime = std::chrono::high_resolution_clock().now();
     for (;;)
     {
         std::shared_ptr<Task> task;
         {
+            std::unique_lock<std::mutex> lock(taskQueueMtx_);
             if (poolMode_ == PoolMode::MODE_CACHED)
             {
-
+                if (!notEmpty_.wait_for(lock, std::chrono::seconds(1),
+                                        [&]()->bool { return taskQueSize_ > 0; }))
+                {
+                    auto timeNow = std::chrono::high_resolution_clock().now();
+                    // int timeDiffer = (int)(timeNow - lastTime); // 不能这么写！
+                    auto dur = std::chrono::duration_cast<std::chrono::seconds>(timeNow - lastTime).count();
+                    if (!isPoolRunning_ || (dur > THREAD_MAX_IDLE_TIME && curThreadSize_ > initThreadSize_))
+                    {
+                        // 线程的unique_ptr生命周期结束则自动析构
+                        threads_.erase(threadid);
+                        curThreadSize_--;
+                        idleThreadSize_--;
+                        std::cout << "threadid: " << std::this_thread::get_id() << "exit!"
+                         << std::endl;
+                        if(!isPoolRunning_)
+                        {
+                            exitCondition_.notify_all();
+                        }
+                        return;
+                    }
+                    continue;
+                }
             }
             else
             {
-                // fixed 模式下 等待任务队列有任务的情况下执行线程函数
-                std::unique_lock<std::mutex> lock(taskQueueMtx_);
-                notEmpty_.wait(lock, [&]()-> { return taskQueSize_ > 0; });
-                --idleThreadSize_;
-                task = taskQueue_.front();
-                taskQueue_.pop();
-                taskQueSize_--;
-
-                // notification
-                if (taskQueSize_ > 0)
+                // fixed
+                notEmpty_.wait(lock, [&]()->bool { return (taskQueSize_ > 0 || !isPoolRunning_); });
+                if (!isPoolRunning_)
                 {
-                    notEmpty_.notify_all();
+                    threads_.erase(threadid);
+                    exitCondition_.notify_all();
+                    return;
                 }
-                notFull_.notify_all();
             }
+            --idleThreadSize_;
+            task = taskQueue_.front();
+            taskQueue_.pop();
+            taskQueSize_--;
+
+            // notification
+            if (taskQueSize_ > 0)
+            {
+                notEmpty_.notify_all();
+            }
+            notFull_.notify_all();
         }
 
         // execution
-        if (!task)
+        if (task)
         {
             task->exec();
             ++idleThreadSize_;
+            lastTime = std::chrono::high_resolution_clock().now();
+        }
+        if (!isPoolRunning_)
+        {
+            threads_.erase(threadid);
+            exitCondition_.notify_all();
+            return;
         }
     }
 }
